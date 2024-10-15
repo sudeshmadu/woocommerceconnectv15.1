f14c4e87c1713acd38230a28acf8b6cfec9aa404
sudeshmadu <104251844+sudeshmadu@users.noreply.github.com>
2024-10-15 11:40:23 +0530

    24101511 woocommerce_requests.py

--- woocommerceconnector/woocommerce_requests.py
+++ woocommerceconnector/woocommerce_requests.py
@@ -176,7 +176,7 @@ def get_woocommerce_items(ignore_filter_conditions=False):
     woocommerce_products.extend(response.json())
 
 for page_idx in range(1, int( response.headers.get('X-WP-TotalPages')) or 1):
-        response = get_request_request('products?per_page={0}&page={1}&{2}'.format(_per_page,page_idx+1,filter_condition) )
+        response = get_request_request('products?per_page={0}&page={1}&{2}'.format(_per_page,page_idx,filter_condition) )
         woocommerce_products.extend(response.json())
 
     return woocommerce_products
